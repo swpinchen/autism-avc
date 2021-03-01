121bbdd6c14303ad8b38b13490cb573e4c18226a
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-01 17:51:33 +0900

    fixed date in reviews to :date

---
+++ db/migrate/20210301084944_change_date_in_review.rb
@@ -0,0 +1,5 @@
+class ChangeDateInReview < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reviews, :date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_081512) do
+ActiveRecord::Schema.define(version: 2021_03_01_084944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_081512) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.datetime "date"
+    t.date "date"
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
