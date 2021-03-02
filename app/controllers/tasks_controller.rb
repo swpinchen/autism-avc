45954d951c891bb1d3fe66b24d7f1a7dd0e6a9f8
Henry Knight <hhknight@ucdavis.edu>
2021-03-02 11:14:31 +0900

    items show and index in action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,27 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update]
+
   def index
+    # @tasks = policy_scope(Task)
     @tasks = Task.all
   end
 
   def show
+  end
+
+  # def new
+  #   @task = Task.new
+  # end
+
+  # def create
+  #   @task = Task.new
+  #   @user = current_user
+  # end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new
-    @user = current_user
-
+    authorize @task
   end
 end
