7752204d4d330d792f168b3033a344e67ba00d0b
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-02 15:48:50 +0900

    seed for items created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,56 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning up db...'
+Item.destroy_all
+puts 'All items deleted'
+User.destroy_all
+puts 'All users deleted.'
 
-# -User Columns -
-#     t.string "name"
-#     t.bigint "parent_id", null: false
-#     t.integer "roles"
-#     t.string "theme"
-#     t.string "username"
-#     t.date "birthday"
 
 puts 'Creating users...'
-user_email_list = ['s_pinchen@hotmail.com', 'ivanwilf@gmail.com', 'natasha030320@gmail.com', 'hhknight@me.com']
+user_email_list = ['s_pinchen@hotmail.com', 'ivanwilf@hotmail.com', 'natasha030320@gmail.com', 'hhknight@me.com']
 user_list = []
 
 user_email_list.length.times do |index|
-  user = User.create!(email: user_email_list[index], password: "password", name: Faker::Name.name, birthday: Date.new(rand(2011..2015), rand(1..12), rand(1..28)))
+  user = User.create!(email: user_email_list[index], password: "password", name: Faker::Name.name, birthday: Date.new(rand(2011..2015), rand(1..12), rand(1..28)), username: user_email_list[index].match(/(\S+)(@)(\S+)/)[1])
   user_list << user
   puts "User #{user.id}: #{user.email} was created!"
+  3.times do
+    item = Item.new(title: "Task name", start_date: DateTime.now, end_date: DateTime.new(2021,rand(4..12), rand(1..30)), category: "task,", details: Faker::Movies::StarWars.quote)
+    item.user = user
+    item.save!
+    puts "Task for #{user.name} created"
+  end
 end
-
-item = Item.new(title:
-start_date: Faker::Time.forward(days: 23, period: :morning),
-end_date: Faker::Time.forward(days: 23, period: :evening),
-deletable: false
-editable: false
-category: "task"
-recurring: false
-completed: false
-details:
-)
-
-# title
-# image
-# start_date
-# child_id
-# details
-# deletable
-# editable
-# category
-# end_date
-# recurring
-# completed
-
-
-
-# clinic = Clinic.create!(name: "Stuart's Dentistry",
-#   location: "Meguro 1-2-4, Meguro, Tokyo",
-#   open_hours: "#{Faker::Time.backward(days: 0, period: :morning, format: :short)} - #{Faker::Time.forward(days: 0, period: :morning)}",
-#   description: "We are better than Ivan's clinic. More than half the patients that come here end up getting better. We are located in the heart of Tokyo!",
-#   # specialities: "general practice",
-#   user: user_list.sample,
-# )
-# file = URI.open('https://images.unsplash.com/photo-1567568443997-19f025355d52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80')
-
