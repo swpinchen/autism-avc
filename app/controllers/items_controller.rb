13121e6491afa0922c1a78059ea8490d5e67e8d5
NatashaSison <natasha030320@gmail.com>
2021-03-03 12:54:12 +0900

    Cloudinary & items create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_task, only: [:show, :update]
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_task, only: %i[show update]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @items = policy_scope(Item)
@@ -9,16 +9,28 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def new
-  #   @item = Item.new
-  # end
+  def new
+    @item = Item.new
+    # authorize @item if current_user.owner
+  end
 
-  # def create
-  #   @item = Item.new
-  #   @user = current_user
-  # end
+  def create
+    @item = Item.create(item_params)
+    @item.user = current_user
+    # authorize @item
+    if @item.save
+      redirect_to item_path
+    else
+      render :new
+    end
+  end
 
   private
+
+  def item_params
+    params.require(:item).permit(:title, :start_date, :category, :details, :end_date, :recurring, :completed,
+                                 :editable, :deletable, :photo)
+  end
 
   def set_task
     @item = Item.find(params[:id])
