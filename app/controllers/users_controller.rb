324409378f9b1727bc71dc2fce2d6549d2df555f
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-08 12:15:13 +0900

    Additional info in show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
   def show
     @user = current_user
+    @item = Item.new
+    @items = Item.all
     authorize @user
   end
 end
