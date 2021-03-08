9b3d9fd02bc2034ab4ff41d1d26eae2fedc4076e
NatashaSison <natasha030320@gmail.com>
2021-03-08 12:37:27 +0900

    Rating favorit card display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @items = Item.all
+    @items = Item.joins(:reviews).where("rating = 4")
     @review = Review.new
   end
-
 end
