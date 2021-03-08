eca35a3d511291dd4f34a2aac682a6ab6541c962
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-08 19:44:27 +0900

    Cleared the console

--- app/javascript/plugins/weather.js
+++ app/javascript/plugins/weather.js
@@ -4,8 +4,6 @@ const showWeather = () => {
     fetch('http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=61aa4137c2a624fd51f9b0b5c814c3ac')
       .then(response => response.json())
       .then((data) => {
-        console.log(data.weather[0].icon);
-        console.log(weather);
         weather.style.backgroundImage = `url(http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`;
       })
     }
